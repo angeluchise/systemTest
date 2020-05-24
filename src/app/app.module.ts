c20eb090aef336564543a0f804c6654fd5fdc9d9
angel perez <angel.per.cho@gmail.com>
2020-05-24 18:18:42 -0400

    add calculations and fix responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { ForPricePipe } from './formatPrice.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForPricePipe
   ],
   imports: [
     BrowserModule,
